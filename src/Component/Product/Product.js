1425b4640dd8d9c407e70efd48f4c576cb5b2276
miraj090 <ambroseffc29@gmail.com>
2022-09-27 08:44:46 +0600

    ema-john

---
+++ src/Component/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+import './Product.css'
+
+const Product = (props) => {
+    const {name , img ,seller , price , ratings} = props.product
+    
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+            <p className='Product-name'>{name}</p>
+            <p>Price: ${price}</p>
+            <p><small>Seller : {seller}</small></p>
+            <p><small>Rating : {ratings} star</small></p>
+            </div>
+            <button onClick={ () =>props.handleAddToCart(props.product)} className='btn-cart'>   
+                <p>Add to cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
